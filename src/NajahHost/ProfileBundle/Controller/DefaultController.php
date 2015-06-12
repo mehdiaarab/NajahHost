110d8139a303de822b5ffb8126249e263d0007d8
mehdi.aarab <a.mehdi911@gmail.com>
2015-06-12 20:09:58 +0100

    finished modifying profile - about

--- src/NajahHost/ProfileBundle/Controller/DefaultController.php
+++ src/NajahHost/ProfileBundle/Controller/DefaultController.php
@@ -5,29 +5,44 @@ namespace NajahHost\ProfileBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use UserBundle\Entity\User;
 
-class DefaultController extends Controller
-{
-    
-    public function profileAction($username)
-    {
-    	$user  = $this->getDoctrine()
-        ->getRepository('UserBundle:User')
-        ->findOneByUsername($username);
+class DefaultController extends Controller {
+
+    public function profileAction($username) {
+        $user = $this->getDoctrine()
+                ->getRepository('UserBundle:User')
+                ->findOneByUsername($username);
         return $this->render('ProfileBundle:Default:index.html.twig', array('user' => $user));
     }
 
-   public function editProfileAction($username)
-    {
+    public function editProfileAction($username) {
 
-    	$user = $this->getDoctrine()
-    	->getRepository('UserBundle:User')
-    	->findOneByUsername($username);
-        
-       
+        $user = $this->getDoctrine()
+                ->getRepository('UserBundle:User')
+                ->findOneByUsername($username);
 
-        return $this->render('ProfileBundle:Default:editProfile.html.twig', array('user' => $user));
+        $form = $this->createForm(new \NajahHost\UserBundle\Form\UserType(), $user);
+        $request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+        $form->handleRequest($request);
 
+        if ($form->isValid()) {
 
-    } 
+            try {
+                $em = $this->getDoctrine()->getManager();
+                $user = $form->getData();
+                $em->persist($user);
+                $em->flush();
+                $session = $this->get('session')->getFlashBag()
+                        ->add('success', 'les modification on été effectuer avec succés');
+                return $this->redirect($this->generateUrl('profile', array('username' => $user->getUsername())));
+            } catch (Exception $ex) {
+                $session = $this->get('session')->getFlashBag()->add('danger'
+                        , 'Erreur lors de la modification du profile');
+                return $this->redirect($this->generateUrl('edit_profile',
+                        array('username' => $user->getUsername())));
+            }
+        }
+
+        return $this->render('ProfileBundle:Default:editProfile.html.twig', array('user' => $user, 'form' => $form->createView()));
+    }
 
 }
