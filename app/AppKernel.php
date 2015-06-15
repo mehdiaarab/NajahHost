03cc30d338eda299fae6230d13e050da4ec575eb
mehdi.aarab <a.mehdi911@gmail.com>
2015-06-15 03:31:13 +0100

    template panel chef projet

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new NajahHost\UserBundle\UserBundle(),
             new NajahHost\ProfileBundle\ProfileBundle(),
+            new NajahHost\ProjectBundle\ProjectBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
