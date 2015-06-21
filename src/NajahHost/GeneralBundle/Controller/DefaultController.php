dedeb8c4621bec903f3d692631841ec000139783
mehdi.aarab <a.mehdi911@gmail.com>
2015-06-21 16:58:10 +0000

    finished fixings, i added chef projet panel, still needs to work on notifications

--- src/NajahHost/GeneralBundle/Controller/DefaultController.php
+++ src/NajahHost/GeneralBundle/Controller/DefaultController.php
@@ -11,4 +11,19 @@ class DefaultController extends Controller
 
 		return $this->render('GeneralBundle:Default:index.html.twig');
 	}
+
+    public function panel_chefAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user=$this->get('security.context')->getToken()->getUser();
+        $projects = $em->getRepository('ProjetBundle:Projet')->findBy(array('chefProjet'=>$user));
+
+        $provider = $this->container->get('fos_message.provider');
+        $threads = $provider->getInboxThreads();
+
+        return $this->render('GeneralBundle:Default:panel_chef_projet.html.twig',
+            array('projects' => $projects, 'threads' => $threads)
+        );
+    }
+    
 }
